c2be1f7ae8f5f2dab3d92127c65dcceb99dfa090
joaogab <joaogabrieldecarvalho.com@gmail.com>
2020-08-05 14:05:46 -0300

    Aula03 | Estiliza página de cadastro - select

--- web/src/pages/teacherForm/index.tsx
+++ web/src/pages/teacherForm/index.tsx
@@ -4,8 +4,10 @@ import PageHeader from '../../components/pageHeader';
 import Input from '../../components/input';
 import warningIcon from '../../assets/images/icons/warning.svg';
 
-import './styles.css';
 import TextArea from '../../components/textArea';
+import Select from '../../components/select';
+
+import './styles.css';
 
 function TeacherForm() {
   return (
@@ -28,7 +30,23 @@ function TeacherForm() {
         <fieldset>
           <legend>Sobre a aula</legend>
 
-          <Input name="subject" label="Matéria" />
+          <Select
+            name="subject"
+            label="Matéria"
+            options={[
+              { value: 'Artes', label: 'Artes' },
+              { value: 'Biologia', label: 'Biologia' },
+              { value: 'Ciências', label: 'Ciências' },
+              { value: 'Educação física', label: 'Educação física' },
+              { value: 'Física', label: 'Física' },
+              { value: 'Geografia', label: 'Geografia' },
+              { value: 'História', label: 'História' },
+              { value: 'Matemática', label: 'Matemática' },
+              { value: 'Português', label: 'Português' },
+              { value: 'Química', label: 'Química' },
+            ]}
+          />
+
           <Input name="cost" label="Custo da sua hora por aula" />
 
         </fieldset>
