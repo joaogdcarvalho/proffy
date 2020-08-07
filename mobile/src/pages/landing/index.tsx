a289b1eb6d21515c8c493976c81ab8b674fbfced
joaogab <joaogabrieldecarvalho.com@gmail.com>
2020-08-07 20:00:54 -0300

    Aula05 | Conecta API, exibe total de conexões na landing page

--- mobile/src/pages/landing/index.tsx
+++ mobile/src/pages/landing/index.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
-import { View, Image, Text, TouchableOpacity } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Image, Text } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { RectButton } from 'react-native-gesture-handler';
+import api from '../../services/api';
 
 import styles from './styles';
 
@@ -12,6 +13,15 @@ import heartIcon from '../../assets/images/icons/heart.png';
 
 function Landing() {
   const { navigate } = useNavigation();
+  const [totalConnections, setTotalConnections] = useState(0);
+
+  useEffect(() => {
+    api.get('connections').then(response => {
+      const { total } = response.data;
+
+      setTotalConnections(total);
+    })
+  }, []);
 
   function handleNavigateToGiveClassesPage() {
     navigate('GiveClasses');
@@ -51,7 +61,7 @@ function Landing() {
       </View>
 
       <Text style={styles.totalConnections}>
-        Total de 285 conexões já realizadas. {' '}
+        Total de {totalConnections} conexões já realizadas. {' '}
         <Image source={heartIcon} />
       </Text>
     </View>
