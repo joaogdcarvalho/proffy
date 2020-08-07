a17a0e7514935eac29832b5668638927b6df8e23
joaogab <joaogabrieldecarvalho.com@gmail.com>
2020-08-07 13:33:25 -0300

    Aula04 | Cria página de aulas

--- mobile/src/routes/AppStack.tsx
+++ mobile/src/routes/AppStack.tsx
@@ -3,15 +3,16 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import Landing from '../pages/landing';
+import GiveClasses from '../pages/giveClasses';
 
 const { Navigator, Screen } = createStackNavigator();
 
 function AppStack() {
   return (
     <NavigationContainer>
-      <Navigator
-        screenOptions={{ headerShown: false }}>
+      <Navigator screenOptions={{ headerShown: false }}>
         <Screen name="Landing" component={Landing}/>
+        <Screen name="GiveClasses" component={GiveClasses} />
       </Navigator>
     </NavigationContainer>
   );
