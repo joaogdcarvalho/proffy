6129d94ea03fd404ad78b4d585b7bbe7866527a2
joaogab <joaogabrieldecarvalho.com@gmail.com>
2020-08-07 18:36:11 -0300

    Aula05 | Cria listagem de professores

---
+++ mobile/src/components/teacherItem/index.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { View, Image, Text } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+
+import heartOutlineIcon from '../../assets/images/icons/heart-outline.png';
+import unfavoriteIcon from '../../assets/images/icons/unfavorite.png';
+import whatsappIcon from '../../assets/images/icons/whatsapp.png';
+
+import styles from './styles';
+
+function TeacherItem() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.profile}>
+        <Image
+          style={styles.avatar}
+          source={{ uri: 'https://discord.com/assets/0e291f67c9274a1abdddeb3fd919cbaa.png'}}
+        />
+
+        <View style={styles.profileInfo}>
+          <Text style={styles.name}>Adriana Gomes</Text>
+          <Text style={styles.subject}>Química</Text>
+        </View>
+      </View>
+
+      <Text style={styles.bio}>
+        Professional pop culture specialist. Internetaholic. Tv trailblazer. Award-winning troublemaker.
+        {'\n'}{'\n'}
+        Falls down a lot.
+      </Text>
+
+      <View style={styles.footer}>
+        <Text style={styles.price}>
+          Preço/hora {'   '}
+          <Text style={styles.priceValue}>R$ 20,00</Text>
+        </Text>
+
+        <View style={styles.buttonsContainer}>
+          <RectButton style={styles.favoriteButton}>
+            <Image source={unfavoriteIcon} />
+          </RectButton>
+
+          <RectButton style={styles.contactButton}>
+            <Image source={whatsappIcon} />
+            <Text style={styles.contactButtonText}>Entrar em contato</Text>
+          </RectButton>
+
+        </View>
+      </View>
+    </View>
+  )
+}
+
+export default TeacherItem;
--- mobile/src/pages/teacherList/index.tsx
+++ mobile/src/pages/teacherList/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, ScrollView } from 'react-native';
+
 import PageHeader from '../../components/pageHeader';
+import TeacherItem from '../../components/teacherItem';
 
 import styles from './styles';
 
@@ -8,6 +10,21 @@ function TeacherList() {
   return (
     <View style={styles.container}>
       <PageHeader title="Proffys disponíveis" />
+
+      <ScrollView
+        style={styles.teacherList}
+        contentContainerStyle={{
+          paddingHorizontal: 16,
+          paddingBottom: 16,
+        }}
+      >
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+
+      </ScrollView>
     </View>
   );
 }
