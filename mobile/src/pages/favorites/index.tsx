9e51bf57ac0afb576915016bd3a1ba820fdccc8a
joaogab <joaogabrieldecarvalho.com@gmail.com>
2020-08-07 14:57:09 -0300

    Aula04 | Cria componente cabeçalho de página

--- mobile/src/pages/favorites/index.tsx
+++ mobile/src/pages/favorites/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { View } from 'react-native';
+import PageHeader from '../../components/pageHeader';
 
 import styles from './styles';
 
 function Favorites() {
-  return <View style={styles.container} />
+  return (
+    <View style={styles.container}>
+      <PageHeader title="Meus proffys favoritos" />
+    </View>
+  )
 }
 
 export default Favorites;
