7a992697c750dc9df36b094a19fb0b1e7492e5bb
joaogab <joaogabrieldecarvalho.com@gmail.com>
2020-08-08 15:29:11 -0300

    Aula05 | Conecta API, busca professores cadastrados conforme critérios

--- mobile/src/components/teacherItem/index.tsx
+++ mobile/src/components/teacherItem/index.tsx
@@ -8,31 +8,43 @@ import whatsappIcon from '../../assets/images/icons/whatsapp.png';
 
 import styles from './styles';
 
-function TeacherItem() {
+export interface Teacher {
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+};
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
   return (
     <View style={styles.container}>
       <View style={styles.profile}>
         <Image
           style={styles.avatar}
-          source={{ uri: 'https://discord.com/assets/0e291f67c9274a1abdddeb3fd919cbaa.png'}}
+          source={{ uri: teacher.avatar }}
         />
 
         <View style={styles.profileInfo}>
-          <Text style={styles.name}>Adriana Gomes</Text>
-          <Text style={styles.subject}>Química</Text>
+          <Text style={styles.name}>{teacher.name}</Text>
+          <Text style={styles.subject}>{teacher.subject}</Text>
         </View>
       </View>
 
       <Text style={styles.bio}>
-        Professional pop culture specialist. Internetaholic. Tv trailblazer. Award-winning troublemaker.
-        {'\n'}{'\n'}
-        Falls down a lot.
+        {teacher.bio}
       </Text>
 
       <View style={styles.footer}>
         <Text style={styles.price}>
           Preço/hora {'   '}
-          <Text style={styles.priceValue}>R$ 20,00</Text>
+          <Text style={styles.priceValue}>R$ {teacher.cost}</Text>
         </Text>
 
         <View style={styles.buttonsContainer}>
--- mobile/src/pages/teacherList/index.tsx
+++ mobile/src/pages/teacherList/index.tsx
@@ -4,15 +4,34 @@ import { BorderlessButton, RectButton } from 'react-native-gesture-handler';
 import { Feather } from '@expo/vector-icons';
 
 import PageHeader from '../../components/pageHeader';
-import TeacherItem from '../../components/teacherItem';
+import TeacherItem, {Teacher} from '../../components/teacherItem';
+import api from '../../services/api';
 
 import styles from './styles';
 
 function TeacherList() {
+  const [teachers, setTeachers] = useState([]);
   const [ isFiltersVisible, setIsFiltersVisible] = useState(false);
+
+  const [subject, setSubject] = useState('');
+  const [week_day, setWeekDay] = useState('');
+  const [time, setTime] = useState('');
 
   function handleToggleFiltersVisible() {
     setIsFiltersVisible(!isFiltersVisible);
+  }
+
+  async function handleFiltersSubmit() {
+    const response = await api.get('classes', {
+      params: {
+        subject,
+        week_day,
+        time,
+      }
+    });
+    
+    setIsFiltersVisible(false);
+    setTeachers(response.data);
   }
 
   return (
@@ -31,6 +50,8 @@ function TeacherList() {
             <Text style={styles.label}>Matéria</Text>
             <TextInput
               style={styles.input}
+              value={subject}
+              onChangeText={text => setSubject(text)}
               placeholder="Qual a matéria?"
               placeholderTextColor="#C1BCCC"
             />
@@ -43,6 +64,8 @@ function TeacherList() {
                 </Text>
                 <TextInput
                   style={styles.input}
+                  value={week_day}
+                  onChangeText={text => setWeekDay(text)}
                   placeholder="Qual o dia?"
                   placeholderTextColor="#C1BCCC"
                 />
@@ -54,13 +77,15 @@ function TeacherList() {
                 </Text>
                 <TextInput
                   style={styles.input}
+                  value={time}
+                  onChangeText={text => setTime(text)}
                   placeholder="Qual horário?"
                   placeholderTextColor="#C1BCCC"
                 />
               </View>
             </View>
 
-            <RectButton style={styles.submitButton}>
+            <RectButton onPress={handleFiltersSubmit} style={styles.submitButton}>
               <Text style={styles.submitButtonText}>Filtrar</Text>
             </RectButton>
 
@@ -75,11 +100,10 @@ function TeacherList() {
           paddingBottom: 16,
         }}
       >
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
+
+        {teachers.map((teacher: Teacher) => {
+          return <TeacherItem key={teacher.id} teacher={teacher} />
+        })}
 
       </ScrollView>
     </View>
