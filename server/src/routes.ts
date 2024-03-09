185ea5ae528072ca891076e736b17382b2334a83
joaogdcarvalho <joaogabrieldecarvalho.com@gmail.com>
2024-03-08 22:17:00 -0300

    fix(server/routes.js): Adiciona middleware de limitação de taxa para rota POST /connections.

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,15 +1,23 @@
-import express from 'express';
-import ClassesControllers from './controllers/ClassesController';
-import ConnectionsController from './controllers/ConnectionsController';
+import express from "express";
+import ClassesControllers from "./controllers/ClassesController";
+import ConnectionsController from "./controllers/ConnectionsController";
+import rateLimit from "express-rate-limit";
 
 const routes = express.Router();
 const classesControllers = new ClassesControllers();
 const connectionsController = new ConnectionsController();
 
-routes.get('/classes', classesControllers.index);
-routes.post('/classes', classesControllers.create);
+const limiter = rateLimit({
+  windowMs: 60 * 1000, // 1 minuto
+  max: 100,
+});
 
-routes.get('/connections', connectionsController.index);
-routes.post('/connections', connectionsController.create);
+routes.use(limiter);
+
+routes.get("/classes", classesControllers.index);
+routes.post("/classes", classesControllers.create);
+
+routes.get("/connections", connectionsController.index);
+routes.post("/connections", connectionsController.create);
 
 export default routes;
